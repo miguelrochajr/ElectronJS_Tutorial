442499fc46bab036db120f5f545f568e9cfa5bdf
Miguel Rocha Jr <miguelsrochajr@gmail.com>
2018-06-02 15:30:10 -0300

    Finished Electron tutorial course

--- convert/index.js
+++ convert/index.js
@@ -1,6 +1,8 @@
 const electron = require('electron');
+const ffmpeg = require('fluent-ffmpeg');
+const _ = require('lodash');
 
-const { app, BrowserWindow, ipcMain } = electron;
+const { app, BrowserWindow, ipcMain, shell } = electron;
 
 let mainWindow;
 
@@ -15,5 +17,42 @@ app.on('ready',() => {
 });
 
 ipcMain.on('videos:added', (event, videos) => {
-    console.log(videos);
+    const promises = _.map(videos, video => {
+        return new Promise((resolve, reject) => {
+            ffmpeg.ffprobe(video.path, (err, metadata) => {
+                resolve({ 
+                    ...video, 
+                    duration: metadata.format.duration, 
+                    format: 'avi'
+                });
+            });
+        });
+    });
+
+    Promise.all(promises)
+        .then((results) => {
+            mainWindow.webContents.send('metadata:complete', results);
+        });
+});
+
+ipcMain.on('conversion:start', (event, videos) => {
+    _.each(videos, video => {
+        const outputDirectory = video.path.split(video.name)[0];
+        const outputName = video.name.split('.')[0];
+        const outputPath = `${outputDirectory}${outputName}.${video.format}`;
+    
+        ffmpeg(video.path)
+            .output(outputPath)
+            .on('progress', ({ timemark }) => 
+                mainWindow.webContents.send('conversion:progress', { video, timemark })
+            )
+            .on('end', () => 
+                mainWindow.webContents.send('conversion:end', { video, outputPath })
+            )
+            .run();
+    });
+});
+
+ipcMain.on('folder:open', (event, outputPath) => {
+    shell.showItemInFolder(outputPath);
 });
